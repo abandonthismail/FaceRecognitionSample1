b620582bc7024f407eafcbe4382ef7e75719ebc1
Kenichi Enomoto <abandon.this.mail@gmail.com>
2026-06-17 04:21:51 +0900

    Add User Management

---
+++ FaceRecognitionSample1/Views/UserManagementView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FaceRecognitionSample1.Views
+{
+    /// <summary>
+    /// Interaction logic for UserManagementView.xaml
+    /// </summary>
+    public partial class UserManagementView : UserControl
+    {
+        public UserManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
